d7ba285a9be4a0b28487f6700d029fa47ed59a6e
Alonso Cauich <alonsocauichv1@gmail.com>
2017-06-10 12:13:10 -0500

    se quito ul del menu
    se quito ul del menu

--- resources/views/layouts/partials/mainheader.blade.php
+++ resources/views/layouts/partials/mainheader.blade.php
@@ -81,10 +81,7 @@
                     </li>
                 @endif
 
-                <!-- Control Sidebar Toggle Button -->
-                <li>
-                    <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
-                </li>
+            
             </ul>
         </div>
     </nav>
