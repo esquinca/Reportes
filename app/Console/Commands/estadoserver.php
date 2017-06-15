31eeb70b20c499c02f97e0b21b78bcb404b70d7f
Unknown <esquinca_jose@hotmail.com>
2017-06-15 11:59:52 -0500

    estado server mail
    corrige fail

--- app/Console/Commands/estadoserver.php
+++ app/Console/Commands/estadoserver.php
@@ -6,6 +6,8 @@ use Illuminate\Console\Command;
 
 use DB;
 
+use Mail;
+
 class estadoserver extends Command
 {
     /**
@@ -44,12 +46,26 @@ class estadoserver extends Command
       //Creo un ciclo for para recorrer las posiciones del array
       for ($i=0; $i < $contar_ip ; $i++) {
         $host=$zoneDirect_sql[$i]->ip;
+        $hostcorreo = DB::table('CorreosZD')->select('Nombre_hotel', 'Correo', 'nombre_itc')->where('ip' , '=', $host)->get();
         exec("ping -c 1 " . $host, $output, $result);
         if ($result == 0){
           //echo "Ping successful!";
           $sql_a= DB::table('zonedirect_ip')
             ->where('ip', '=', $host)
             ->update(['status' => 1]);
+
+          $nombrehotel = $hostcorreo[0]->Nombre_hotel; 
+          $nombreit = $hostcorreo[0]->nombre_itc; 
+          $correoit = $hostcorreo[0]->Correo; 
+ 
+          $data = [ 
+            'ip' => $host, 
+            'hotel' => $nombrehotel, 
+            'nombre' => $nombreit 
+          ];
+
+          $this->enviarC($correoit, $nombreit, $data);
+
         }
         else {
           //echo "Ping unsuccessful!";
@@ -59,5 +75,13 @@ class estadoserver extends Command
         }
       }
       $this->info('Updated ip address states successfully!');
+    }
+
+    public function enviarC($correo, $nombre, $datos) 
+    { 
+      Mail::send('email', $datos, function ($message) use ($correo, $nombre) { 
+          //$message->from('contactoweb@sitwifi.com', 'ContactoSitwifiWeb'); 
+          $message->to($correo, $nombre)->subject('Reportes Diarios - Acceso denegado'); 
+      }); 
     }
 }
